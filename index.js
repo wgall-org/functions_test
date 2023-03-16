cfbc56e7bb722b2002631623566ca1227918236f
wgall <106452389+wgall@users.noreply.github.com>
2023-03-16 22:48:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /**
  * Responds to any HTTP request.
  *
- * @param {!express:Request} req HTTP request context.
- * @param {!express:Response} res HTTP response context.
+  @param {!express:Request} req HTTP request context.
+  @param {!express:Response} res HTTP response context.
  */
 exports.main = (req, res) => {
   let message = req.query.message || req.body.message || 'Hello World!';
