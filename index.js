8a61e1e6b51b16ae43a41778879826fdd623070c
Wojciech Gall <106452389+wgall@users.noreply.github.com>
2024-02-05 16:11:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 exports.main = (req, res) => {
-  let message = req.query.message || req.body.message || 'Hello World!';
+  let message = req.query.message || req.body.message || 'Hello World!'; 
   res.status(200).send(message);
 };
